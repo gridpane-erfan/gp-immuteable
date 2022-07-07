d275860b899b57456c3f03f62b5d3b2419e9e2e2
Erfan Ilyas <repo-bot@waas1.com>
2022-07-07 10:29:51 +0500

    removed

--- wp-content/plugins/advanced-posts-blocks/build/blocks/children/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a49f7b923767ee64a7e9855afb7c856d');
